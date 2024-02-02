e892c814509fe2781a75cdd8dd13248d2b27beb0
khalilfarouqi <bio.khalil.f@gmail.com>
2024-02-02 18:17:41 +0100

    show all data excist in admin

--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from . models import Page
 
-def index_pages(request):
-    return render(request, "pages/page.html")
+def index_pages(request, pagename):
+    pagename = '/' + pagename
+    pg = Page.objects.get(permalink=pagename)
+    context = {
+        'title': pg.title,
+        'content': pg.bodytext,
+        'last_updated': pg.update_date,
+    }
+    return render(request, "pages/page.html", context)
